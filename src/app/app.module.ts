92e549c1428d02812149bc827244a3389a77256d
Yoan <yoanmalaver@gmail.com>
2023-05-06 18:11:44 -0500

    Añadido componentes Home, About, Galery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { GaleryComponent } from './components/galery/galery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    GaleryComponent
   ],
   imports: [
     BrowserModule
